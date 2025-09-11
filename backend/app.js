15691af52b2cc31114dabcc0a1075e3e58f78379
Nazmul Limon <nazmulhossenlimon56@gmail.com>
2025-09-11 12:18:12 +0600

    Add backend app.js and related files

--- backend/app.js
+++ backend/app.js
@@ -49,7 +49,7 @@ app.use("/api/v1", paymentRoutes);
 if (process.env.NODE_ENV === "PRODUCTION") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
   });
 }
